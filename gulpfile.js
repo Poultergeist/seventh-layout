86870f95e4f1b3e92083ec196a862d7076670814
kisirai <vlogatskiy3@gmail.com>
2024-09-09 13:12:02 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const path = require('path');
+const through2 = require('through2');
+
+function addFileHeader() {
+  return through2.obj(function(file, enc, cb) {
+    if (file.isBuffer()) {
+      const headerComment = `\n\n/* ${path.basename(file.path)} */\n`;
+      file.contents = Buffer.concat([
+        Buffer.from(headerComment),
+        file.contents
+      ]);
+    }
+    cb(null, file);
+  });
+}
+
+gulp.task('sass', function() {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(addFileHeader())
+    .pipe(concat('index.css'))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
+});
+
+gulp.task('default', gulp.series('sass', 'watch'));
