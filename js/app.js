701ba614a3eb81b8d00dad56a6336065cf6dc3a7
kisirai <vlogatskiy3@gmail.com>
2024-09-10 17:57:09 +0300

    banner completed, make adaptive

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('.banner-slider').slick({
+    arrows: false,
+    adaptiveHeight: false,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    infinite: true
+  });
+});
