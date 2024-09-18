2f01d371dc7edc8cf68c9fb4e6f18b75f41f7ddf
kisirai <vlogatskiy3@gmail.com>
2024-09-18 14:19:46 +0300

    add footer, header, navbar, made adaptive

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
     autoplay: false,
     infinite: true,
     fade: true,
+    zIndex: 3,
     asNavFor: ".share__slider"
   });
 
@@ -94,3 +95,45 @@ likes.forEach(like => {
     likeCountElement.textContent = likeCount;
   })
 });
+
+$('#burger-button').on('click', function() {
+  $('.navbar-collapse').toggleClass('collapse');
+  $(this).toggleClass('burger-active');
+  $('.header').toggleClass('drop-shadow-expand');
+});
+
+window.addEventListener("scroll", scrollCheck);
+
+function scrollCheck() {
+  const targets = document.getElementsByClassName("jump-block");
+  for (i = 0; i < targets.length; i++) {
+    targetsVisibilityCheck(targets[i], targets[i - 1], targets[i + 1])
+  }
+}
+
+setTimeout(scrollCheck, 10);
+
+function targetsVisibilityCheck(target, prevTarget, nextTarget) {
+  const rect = target.getBoundingClientRect();
+  const prevRect = prevTarget != undefined ? prevTarget.getBoundingClientRect() : 0;
+  const nextRect = nextTarget != undefined ? nextTarget.getBoundingClientRect() : 0;
+
+  if (prevTarget != undefined && nextTarget != undefined)
+    changeStyle(target.id, (prevRect.bottom < 100 && rect.bottom >= 100 && nextRect.top > 100));
+
+  if (prevTarget == undefined && nextTarget != undefined)
+    changeStyle(target.id, (rect.bottom >= 100 && nextRect.top > 100));
+
+  if (prevTarget != undefined && nextTarget == undefined)
+    changeStyle(target.id, (prevRect.bottom < 100 && rect.bottom >= 100));
+}
+
+
+function changeStyle(targetId, result) {
+  if (result) {
+    document.getElementById(`${targetId}-ancor`).classList.add("active");
+  }
+  else {
+    document.getElementById(`${targetId}-ancor`).classList.remove("active");
+  }
+}
